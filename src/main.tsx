9d2145c27679d268779f90529e7104f0b7228655
davidgbrils <dvdgbriel1@gmail.com>
2026-04-08 11:54:33 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import "@fontsource/poppins";
-import "@fontsource/quicksand";
+import "@fontsource/poppins/index.css";
+import "@fontsource/quicksand/index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
